1213428021aaf6474e2cfabb7e9dcaccac849f0a
IAXRetailer <h2sxxa0w0@gmail.com>
2023-02-25 01:20:51 +0800

    :construction: Research on Shoot danmaku

--- src/main/java/io/github/h2sxxa/gensokyom/items/weapon/GossipStove.java
+++ src/main/java/io/github/h2sxxa/gensokyom/items/weapon/GossipStove.java
@@ -1,9 +1,7 @@
 package io.github.h2sxxa.gensokyom.items.weapon;
 
 import io.github.h2sxxa.gensokyom.items.CanShotBase;
-import io.github.h2sxxa.gensokyom.utils.DanmakuUtils;
 import net.katsstuff.teamnightclipse.danmakucore.DanmakuCore;
-import net.katsstuff.teamnightclipse.danmakucore.danmaku.DanmakuState;
 import net.katsstuff.teamnightclipse.danmakucore.danmaku.DanmakuTemplate;
 import net.katsstuff.teamnightclipse.danmakucore.lib.LibColor;
 import net.katsstuff.teamnightclipse.danmakucore.lib.data.LibShotData;
@@ -13,9 +11,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class GossipStove extends CanShotBase {
     public GossipStove(String name, CreativeTabs tab) {
@@ -26,18 +22,20 @@ public class GossipStove extends CanShotBase {
     public void onShotUse(World worldIn, EntityPlayer player, EnumHand handIn) {
         super.onShotUse(worldIn, player, handIn);
         DanmakuTemplate.Builder temp = DanmakuTemplate.builder()
+                .setSource(player)
                 .setUser(player)
                 .setWorld(worldIn)
-                .setSource(player)
-                .setPos(DanmakuUtils.genLaserVec(player))
-
+                .setMovementData(0D)
                 .setShot(LibShotData.SHOT_LASER
                         .setCoreColor(LibColor.COLOR_VANILLA_WHITE)
                         .setEdgeColor(LibColor.COLOR_SATURATED_YELLOW)
                         .setDamage(2f)
                         .scaleSize(10f)
                 );
-
+        temp.setPos(new Vector3(player).offset(
+                new Vector3(player).normalize(),
+                30)
+        );
         DanmakuCore.spawnDanmaku(Collections.singletonList(temp.build().asEntity()));
     }
 }
--- src/main/java/io/github/h2sxxa/gensokyom/utils/DanmakuUtils.java
+++ src/main/java/io/github/h2sxxa/gensokyom/utils/DanmakuUtils.java
@@ -1,16 +1,10 @@
 package io.github.h2sxxa.gensokyom.utils;
 
-import io.github.h2sxxa.gensokyom.subentity.GSKMSubenetites;
 import net.katsstuff.teamnightclipse.danmakucore.DanmakuCore;
 import net.katsstuff.teamnightclipse.danmakucore.danmaku.DanmakuState;
 import net.katsstuff.teamnightclipse.danmakucore.danmaku.DanmakuTemplate;
 import net.katsstuff.teamnightclipse.danmakucore.danmaku.form.Form;
-import net.katsstuff.teamnightclipse.danmakucore.danmaku.subentity.SubEntity;
-import net.katsstuff.teamnightclipse.danmakucore.danmaku.subentity.SubEntityType;
 import net.katsstuff.teamnightclipse.danmakucore.data.ShotData;
-import net.katsstuff.teamnightclipse.danmakucore.lib.LibColor;
-import net.katsstuff.teamnightclipse.danmakucore.lib.data.LibForms;
-import net.katsstuff.teamnightclipse.mirror.client.shaders.UniformType;
 import net.katsstuff.teamnightclipse.mirror.data.Vector3;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -40,7 +34,7 @@ public class DanmakuUtils {
 
     public static Vector3 genLaserVec(Entity entity){
         Vector3 pos_start = Vector3.fromEntityCenter(entity);
-        pos_start.multiply(40f);
+        pos_start.multiply(5f);
         return pos_start;
     }
 }
